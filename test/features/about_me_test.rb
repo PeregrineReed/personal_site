1199ad34d60a7451ace2532291621d1ea9441e67
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-12 19:51:28 -0700

    Created test for about me page

---
+++ test/features/about_me_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+
+class AboutMeTest < CapybaraTestCase
+  def test_it_has_an_about_me_page
+    visit '/about'
+
+    assert page.has_content?("About Me!\nHere's some stuff to know.")
+    assert_equal 200, page.status
+  end
+end
